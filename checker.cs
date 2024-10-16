04e82f82bf1a9983ff9f9d50df1f5c94a4be4637
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 18:47:45 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -15,8 +15,8 @@ static void Main()
 {
     BtteryParameterChecker checker = new BatteryParameterChecker(warningConfig);
     
-    ExpectTrue(batteryIsOk(25,70,0.7f));
-    ExpectFalse(batteryIsOk(50,85,0.0f));
+    ExpectTrue(batteryIsOk(25,70,0.7f));      //values within range
+    ExpectFalse(batteryIsOk(50,85,0.0f)); //values not within range
     Console.WriteLine("All ok");
 }
    static void ExpectTrue(bool expression)
