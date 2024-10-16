a9e2a15c4794bec406aeb46be3aaff9921253e34
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 19:01:05 +0530

    Update CheckBatteryParameters.cs

--- CheckBatteryParameters.cs
+++ CheckBatteryParameters.cs
@@ -6,4 +6,10 @@ namespace paradigm_shift_csharp
   {
     static void Main()
     {
-      
+      Check_StatusoOfBattery BatteryParameters = new Check_StatusoOfBattery();
+      BatteryParmeters.ExpectTrue(Check_StatusoOfBattery.BatteryIsOk(25,70,0.7f));
+      BatteryParmeters.ExpectFalse(Check_StatusoOfBattery.BatteryIsOk(50,85,0.0f));
+      Console.WriteLine("AllChecks passed!");
+    }
+  }
+}
