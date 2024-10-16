6adde1b917f02e24711cd5e7b41169d3082c1054
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 18:39:47 +0530

    Create ConfigureWarning.cs

---
+++ ConfigureWarning.cs
@@ -0,0 +1,9 @@
+namespace paradigm_shift_csharp
+{
+  class WarningConfig
+  {
+    public float TempWarningThreshold{get;set;}
+    public float SocWarningThreshold{get;set;}
+    public float ChargeRateWarningThreshold{get;set;}
+  }
+}
