f45ce510e177a181002c9a468c99545122d738dd
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 17:54:30 +0530

    Create CheckBatteryParameters.cs

---
+++ CheckBatteryParameters.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace paradigm_shift_csharp
+{
+  class BatteryParameterChecker
+  {
+    private readonly WarningConfig_warningConfig;
+    public BatteryParameterChecker(WarningConfig wqarningConfig)
+    {
+      _warningConfig = warningConfig;
+    }
+    public bool batteryIsOk(float temp,float soc,float chargerate)
+    {
+      return Temp_Ok(temp) && soc_ok(soc) && chargerate_ok(chargerate);
+    }
+    private bool Temp_ok(float tremp)
+    {
+      if(temp <0|| temp>45)
+      {
+        Console.WriteLine("Temperature is out of range!");
+        return false;
+      }
+      else if(temp>40)
+      {
+        Console.WriteLine("Warning :Approaching temperature limit!");
+      }
+      return true;
+    }
+
+    private bool soc_ok(float soc)
+    {
+      if(soc<20 || soc>80)
+      {
+        Console.WriteLine("State of Charge is out of range!");
+        return false;
+      }
+      else if (soc<24)
+      {
+        Console.WriteLine("Warning:Appproaching discharge!");
+      }
+      else if(soc >76)
+      {
+        Console.WriteLine("Warning:Approaching charge-peak!");
+      }
+      return true;
+    }
+
+private bool chargerate_ok(float chargerate)
+{
+  if(chargerate > 0.8)
+  {
+    Console.WriteLine("Charge rate is out of range!");
+    return false;
+  }
+  else if (chargerate > 0.76f)
+  {
+    Console.WriteLine("Warning:Approaching charge rate limit!");
+  }
+  return true;
+}
+}
+}
+
+        
