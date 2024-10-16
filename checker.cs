d799bcec558fd0aa1c723d36ea353b939396cf38
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 22:57:35 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -2,36 +2,14 @@ using System;
 
 namespace ParadigmShiftCSharp
 {
-    class BatteryStatusCheck
+    class Checker
     {
-        private const float SocMin = 20f;
-        private const float SocMax = 80f;
-        private const float TempMin = 0f;
-        private const float TempMax = 45f;
-        BatteryChecks BatteryLimituse = new BatteryChecks();
-        public void ExpectTrue(bool expression)
+        static void Main()
         {
-            if (!expression)
-            {
-                Console.WriteLine("Expected true, but got false");
-                Environment.Exit(1);
-            }
-        }
-
-        public void ExpectFalse(bool expression)
-        {
-            if (expression)
-            {
-                Console.WriteLine("Expected false, but got true");
-                Environment.Exit(1);
-            }
-        }
-
-        public static bool BatteryIsOk(float temperature, float soc, float chargeRate)
-        {
-            return BatteryChecks.LimitCheck(temperature, TempMin, TempMax, "Temperature", true) &&
-                   BatteryChecks.LimitCheck(soc, SocMin, SocMax, "SoC", true) &&
-                   BatteryChecks.ChargeRateCheck(chargeRate);
+            BatteryStatusCheck BatteryMonitor = new BatteryStatusCheck();
+            BatteryMonitor.ExpectTrue(BatteryStatusCheck.BatteryIsOk(25, 70, 0.7f));
+            BatteryMonitor.ExpectFalse(BatteryStatusCheck.BatteryIsOk(50, 85, 0.0f));
+            Console.WriteLine("All checks passed!");
         }
     }
 }
