aec5533a239ecf1a5c7186fb140ea32657d49a28
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 19:20:24 +0530

    Update and rename ConfigureWarning.cs to Checkbatterylimit.cs

--- ConfigureWarning.cs
+++
@@ -1,9 +0,0 @@
-namespace paradigm_shift_csharp
-{
-  class WarningConfigure 
-  {
-    public float TempWarningThreshold{get;set;}
-    public float SocWarningThreshold{get;set;}
-    public float ChargeRateWarningThreshold{get;set;}
-  }
-}
