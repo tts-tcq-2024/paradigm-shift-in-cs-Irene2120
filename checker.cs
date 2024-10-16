a062439c6f5f6a9b06d2ab89ece7c64b782b054b
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-10-16 23:52:41 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace ParadigmShiftCSharp
 {
-    class Checker
+    class Program
     {
         static void Main()
         {
-            BatteryStatusCheck BatteryMonitor = new BatteryStatusCheck();
-            BatteryMonitor.ExpectTrue(BatteryStatusCheck.BatteryIsOk(25, 70, 0.7f));
-            BatteryMonitor.ExpectFalse(BatteryStatusCheck.BatteryIsOk(50, 85, 0.0f));
+            var statusChecker = new BatteryStatusChecker();
+            statusChecker.AssertTrue(BatteryStatusChecker.IsBatteryHealthy(25, 70, 0.7f));
+            statusChecker.AssertFalse(BatteryStatusChecker.IsBatteryHealthy(50, 85, 0.0f));
             Console.WriteLine("All checks passed!");
         }
     }
